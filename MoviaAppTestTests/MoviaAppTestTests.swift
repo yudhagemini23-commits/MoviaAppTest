7bbf48596243aaafd4abce6e64f74de60a0b4183
Yudha <yudha.pp23@gmail.com>
2026-02-04 00:20:01 +0700

    Initial Commit

---
+++ MoviaAppTestTests/MoviaAppTestTests.swift
@@ -0,0 +1,17 @@
+//
+//  MoviaAppTestTests.swift
+//  MoviaAppTestTests
+//
+//  Created by Yudha Pratama Putra on 2/4/26.
+//
+
+import Testing
+@testable import MoviaAppTest
+
+struct MoviaAppTestTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
